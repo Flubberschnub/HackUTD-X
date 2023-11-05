9994d9e9df5dc85067b76e8f66ec9375a82bc1fc
KLin123 <karen234765@gmail.com>
2023-11-04 20:51:39 -0500

    frontEnd

---
+++ frontEnd/investment/src/App.js
@@ -0,0 +1,36 @@
+function App() {
+  return <top>
+    {/* <div1>
+      <h1>Money: </h1>
+      <h1>Net Worth: </h1>
+    </div1>
+    <div2>
+      <h1>Stocks: </h1>
+      <h1>Bonds: </h1>
+      <h1>Certificate of Deposits (CDs): </h1>
+      <h1>Mutual Funds: </h1>
+    </div2>
+    <div3>
+      <h1>Year (Insert year) of 4</h1>
+      <h1>Month (Insert Month)</h1>
+    </div3> */}
+    <div class="row">
+      <div class="column">
+        <h1>Money: </h1>
+        <h1>Net Worth: </h1>
+      </div>
+      <div class="column">
+        <h1>Stocks: </h1>
+        <h1>Bonds: </h1>
+        <h1>Certificate of Deposits (CDs) ikjdflasjdfl;asdlfkjs: </h1>
+        <h1>Mutual Funds: </h1>
+      </div>
+      <div class="column">
+        <h1>Year (Insert year) of 4</h1>
+        <h1>Month (Insert Month)</h1>
+      </div>
+    </div>
+  </top>;
+}
+
+export default App;
